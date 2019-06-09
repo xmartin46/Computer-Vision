fbf85d8a4c95bc751befd9f43bfc2f644ba45353
adria <adriacabeza@hotmail.com>
2019-06-09 10:12:58 +0200

    issue for the segmented solved

--- Practica2/script.py
+++ Practica2/script.py
@@ -19,14 +19,14 @@ for root, dirs, files in os.walk("./Segmentades",topdown=False):
             #print(name)
             img = Image.open(os.path.join(root, name))
             data = numpy.array(img)
-            #aug = HorizontalFlip()
+            aug = HorizontalFlip(p=1)
             #aug = RandomRotate90()
             #image = applyAugmentation(aug,data)
             #aug = Blur(blur_limit=12, p= 1)
             #image = applyAugmentation(aug,image)
             #aug = RandomBrightnessContrast(0.8)
             #aug = RandomScale(scale_limit=1,p=1)
-            aug = RandomCrop(height = 400, width = 400, always_apply=True)
+            #aug = RandomCrop(height = 400, width = 400, always_apply=True)
             image = applyAugmentation(aug,data)
             #aug = IAASharpen()
             #image = applyAugmentation(aug,image)
@@ -38,7 +38,7 @@ for root, dirs, files in os.walk("./Segmentades",topdown=False):
            #     shutil.rmtree(os.path.join(root,'Augmentation')) 
             nom = os.path.join(root,'Augmentation',name)
             nom = nom[:-4]
-            nom = nom + "_scale.jpg"
+            nom = nom + "_horizontal.jpg"
             #print(nom)
             im.save(nom)
             
@@ -49,11 +49,11 @@ for root, dirs, files in os.walk("./Segmentades",topdown=False):
                 print(segmented)
 
                 img = imageio.imread(os.path.join(root, segmented))
-                image = applyAugmentation(aug,data)
+                img = applyAugmentation(aug,img)
             
                 nom = os.path.join(root,'Augmentation',segmented)
                 nom = nom[:-4]
-                nom = nom + "_scale.png"
+                nom = nom + "_horizontal.png"
                 #print(nom)
                 imageio.imwrite(nom, img[:,:,:])
             except:
