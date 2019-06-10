398022642234f6df1070d59914aa1da34db6b5e4
adria <adriacabeza@hotmail.com>
2019-06-10 20:55:09 +0200

    esto va tardar

--- HOME/getImage.m
+++ HOME/getImage.m
@@ -1,14 +1,16 @@
 function [imageJPG, imagePNG, b] = getImage(code)
-    JPG = ['image_', num2str(code, '%04d'), '.jpg'];
-    PNG = ['image_', num2str(code, '%04d'), '.png'];
+    %code
+    JPG = [code, '.jpg'];
+    PNG = [code, '.png'];
     
     try
         imageJPG = imread(JPG);
         imagePNG = imread(PNG);
         b = 1;
     catch
-        imageJPG = 0;
-        imagePNG = 0;
+        imageJPG = imread(JPG);
+        imagePNG = segment(imageJPG);
+        imagePNG = bwlabel(imagePNG);
         b = 0;
 end
 
--- HOME/ordenatibonic.m
+++ HOME/ordenatibonic.m
@@ -15,7 +15,7 @@ for i = 1:length(Flower_types)
     Image_name = convertCharsToStrings(Flower_types(i).name);
     
     for k=1:floor(length(Images_flower_type) * split)
-        [color, segmented, b] = getImage(Images_flower_type(k).name(7:end-4));
+        [color, segmented, b] = getImage(Images_flower_type(k).name(1:end-4));
 
         if b
             featuresaux = getFeatures(color, segmented);
@@ -28,13 +28,35 @@ for i = 1:length(Flower_types)
     end
 end
 
-classifier = fitctree(featureFlowers, label,"Prune","on","Surrogate","all","MinLeafSize",4);
+for i = 1:length(Flower_types)
+    Images_flower_type = dir(fullfile('Segmentades',Flower_types(i).name,'/Augmentation/*.png'));
+    Image_name = convertCharsToStrings(Flower_types(i).name);
+    
+    for k=1:floor(length(Images_flower_type) * split)
+        [color, segmented, b] = getImage(Images_flower_type(k).name(1:end-4));
+        
+        if b
+            segmented = rgb2gray(segmented);
+            segmented = bwlabel(segmented);
+            segmented(segmented==1) = 3;
+            segmented(segmented==2)=1;
+            featuresaux = getFeatures(color, segmented);
+            featureFlowers = [featureFlowers; featuresaux]; % El MATLAB diu que va lent
+            label = [label;imageName2num(convertCharsToStrings(Image_name))]; % El MATLAB diu que va lent
+            l = l + 1;
+        end
+        j = j + 1;
+        [j l]
+    end
+end
+
+%classifier = fitctree(featureFlowers, label,"Prune","on","Surrogate","all","MinLeafSize",4);
 % view(classifier,"Mode","graph")
 
 % classifier = TreeBagger(50,featureFlowers,label,'OOBPrediction','On',...
 %                                                 'Method','classification')
 
-% classifier = fitcensemble(featureFlowers,label, 'Method', 'Bag')
+classifier = fitcensemble(featureFlowers,label,'OptimizeHyperparameters','auto', 'Method', 'Bag')
 
 
 
